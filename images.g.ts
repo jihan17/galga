39c94eee64101b3296d269504d88b254364181ea
jihan17 <91340172+jihan17@users.noreply.github.com>
2021-10-06 13:39:10 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "kind":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . 4 4 4 4 . . . . . . 
+. . . . . 4 1 1 1 1 4 4 . . . . 
+. . . . . 1 1 1 1 1 1 4 . . . . 
+. . . . . 4 1 1 1 1 1 4 . . . . 
+. . . . . 4 4 1 1 1 1 1 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 1 4 1 4 4 
+4 4 1 1 1 1 1 1 1 1 4 4 4 1 4 4 
+4 4 1 1 1 1 1 1 1 1 1 1 1 1 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 1 1 1 1 1 1 4 1 1 1 1 1 1 4 4 
+4 1 1 1 1 1 1 4 1 1 1 1 1 1 4 4 
+4 1 1 1 1 1 1 1 1 1 1 1 1 1 4 4 
+4 1 1 1 1 1 1 4 1 1 1 1 1 1 4 4 
+4 1 1 1 1 1 1 4 1 1 1 1 1 1 4 4 
+4 1 1 1 1 1 1 4 1 1 1 1 1 1 4 4 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
